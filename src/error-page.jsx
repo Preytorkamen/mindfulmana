457af2c87475069e0bee886eea0d34944a75ea95
Preytorkamen <preytorkamen@icloud.com>
2025-11-12 15:37:01 -0500

    feat: set up Landing Page UI

--- src/error-page.jsx
+++ src/error-page.jsx
@@ -8,8 +8,8 @@ export default function ErrorPage() {
 
   return (
     <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
+      <h1>Hi!</h1>
+      <p>This page has not been built yet.</p>
       <p>
         <i>{error.statusText || error.message}</i>
       </p>
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -2,6 +2,7 @@ import { Link, Outlet } from "react-router-dom";
 import Header from '../components/Header.jsx';
 
 import '../styles/index.css';
+import '../styles/root.css';
 import '../styles/selection.css';
 import '../styles/meditation-card.css';
 
@@ -9,29 +10,13 @@ import '../styles/meditation-card.css';
 export default function Root() {
   return (
     <div>
-      {/* Header / Navbar */}
       <Header title="Mindful Mana — Daily Meditations" />
-        <h1 className="text-xl font-semibold text-gray-800">
-          <Link to="selection">Meditations</Link>
-        </h1>
-        <nav>
-          <Link
-            to="/about"
-            className="text-sm font-medium bg-white px-4 py-2 rounded-full shadow-sm hover:shadow-md transition"
-          >
-            About
-          </Link>
-        </nav>
-
-      {/* Page Content */}
-      <main className="flex-grow p-8">
-        <Outlet />
-      </main>
-
-      {/* Optional Footer */}
-      <footer className="text-center text-sm text-gray-500 py-4">
-        © {new Date().getFullYear()} Meditation App
-      </footer>
+        <div className="monk-image" >
+          <img src="../src/assets/Monk.png" alt="MonkImage"></img>
+        </div>
+        <button className="large-ghost">
+          <Link to="selection">Begin Meditation</Link>
+        </button>
     </div>
   );
 }
